d6f985979863052071c87659ff65124b44c172e4
Ben <benbperry1@gmail.com>
2022-01-10 17:21:43 -0800

    commit tree aedcd6a0affd6fe12795a7a2c6454e855cd80225

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+
+
+    def index
+        students = Student.all
+        render json: students
+      end
+      
+      def grades
+        grades = Student.all.order(grade: :desc)
+        render json: grades
+      end
+
+      def highest_grade
+        render json: Student.all.order(grade: :desc).first
+      end
+
+
+end
